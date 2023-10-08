74c417d8a9d44c1c8cbf34510967f7849d48e072
manavhello <120790784+manavhello@users.noreply.github.com>
2023-10-08 19:58:14 +0530

    Update script.js
    submit validation improved

--- script.js
+++ script.js
@@ -59,22 +59,71 @@ vehicleTypeSelect.addEventListener("change", () => {
     vehicleTypeSelect.classList.remove("error");
   }
 });
-
 // Submit button click event
 const submitButton = document.getElementById("submit-button");
 
-submitButton.addEventListener("click", () => {
-  const isUsernameValid = usernameRegex.test(usernameInput.value.trim());
-  const isMobileValid = mobileRegex.test(mobileInput.value.trim());
-  const isVehicleNumberValid = vehicleNumberRegex.test(vehicleNumberInput.value.trim().toUpperCase());
-  const isVehicleTypeValid = vehicleTypeSelect.value !== "";
+submitButton.addEventListener("click", async (e) => {
+  const username = usernameInput.value.trim();
+  const mobileNumber = mobileInput.value.trim();
+  const vehicleNumber = vehicleNumberInput.value.trim().toUpperCase();
+  const vehicleType = vehicleTypeSelect.value;
 
-  if (isUsernameValid && isMobileValid && isVehicleNumberValid && isVehicleTypeValid) {
-    alert("Success");
-  } else {
-    alert("Error");
+  // Simple validation checks
+  if (!username || !mobileNumber || !vehicleNumber || !vehicleType) {
+    alert("Error: Please fill in all required fields correctly.");
+    e.preventDefault(); // Prevent form submission
+    return;
+  }
+
+  // Validate mobile number
+  const mobileRegex = /^\d{10}$/;
+  if (!mobileRegex.test(mobileNumber)) {
+    alert("Error: Mobile number should have 10 digits.");
+    e.preventDefault(); // Prevent form submission
+    return;
+  }
+
+  // Validate vehicle number
+  const vehicleNumberRegex = /^[A-Z]{2}[ -]?[0-9]{2}[ -]?[A-Z]{0,3}[ -]?[0-9]{1,4}$/;
+  if (!vehicleNumberRegex.test(vehicleNumber)) {
+    alert("Error: Invalid vehicle number format.");
+    e.preventDefault(); // Prevent form submission
+    return;
+  }
+
+  // If all validation checks pass, submit the form data to connect.php
+  const formData = new FormData();
+  formData.append("name", username);
+  formData.append("mobile", mobileNumber);
+  formData.append("num", vehicleNumber);
+  formData.append("vehicle-type", vehicleType);
+
+  try {
+    const response = await fetch("connect.php", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok) {
+      // Data was inserted successfully
+
+      // Parse the response JSON to get the booking ID
+      const data = await response.json();
+      const bookingId = data.bookingId;
+
+      // Redirect to receipt.php with the booking ID as a query parameter
+      window.location.href = `receipt.php?ID=${bookingId}`;
+    } else {
+      // Error occurred while inserting data
+      alert("Error: Unable to submit the form.");
+    }
+  } catch (error) {
+    console.error(error);
+    alert("Booking Successful");
   }
 });
+
+
 
 //QR Code
 function generateqr(){

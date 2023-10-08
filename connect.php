a225954adaa224cf03fb465d3eb2d1a5fad8e4a4
manavhello <120790784+manavhello@users.noreply.github.com>
2023-10-08 16:36:43 +0530

    Update connect.php
    receipt.php redirected

--- connect.php
+++ connect.php
@@ -1,35 +1,40 @@
+
 <?php
-if (isset($_POST['submit'])){
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+if (isset($_POST['submit'])) {
     $host = 'localhost';
     $user = 'root';
     $pwd = '';
     $db = 'booking';
-    echo 'one';
-   $con = new mysqli($host, $user, $pwd, $db);
+    $con = new mysqli($host, $user, $pwd, $db);
 
-    if ($con->connect_error){
-      die('Connection failed : ' . $con->connect_error);
-   }
-    
+    if ($con->connect_error) {
+        die('Connection failed : ' . $con->connect_error);
+    }
+
     $username = $_POST["name"];
-    
     $mobile = $_POST['mobile'];
-    
     $vehicle = $_POST['num'];
-   
     $type = $_POST['vehicle-type'];
-    
-    
-    
-   $sql0 = "INSERT INTO user (FullName, PhoneNumber, VehicleNumber, VehicleType) VALUES ('$username', '$mobile', '$vehicle', '$type')";
-    
+
+    $sql0 = "INSERT INTO user (FullName, PhoneNumber, VehicleNumber, VehicleType) VALUES ('$username', '$mobile', '$vehicle', '$type')";
+
     if ($con->query($sql0) === TRUE) {
-        echo "";
+        // Data was inserted successfully
+
+        // Retrieve the auto-generated booking ID
+        $bookingId = $con->insert_id;
+
+        // Close the database connection
+        $con->close();
+
+        // Redirect to receipt.php with the booking ID as a query parameter
+        header("Location: receipt.php?ID=" . $bookingId);
+        exit;
     } else {
         echo "Error: " . $sql0 . "<br>" . $con->error;
-   }
-
-    $con->close(); 
+    }
 }
 ?>
-
